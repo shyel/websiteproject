61d1f37a3ae5ab940caa52f942b65504c544b694
shyel <trimmermichele@gmail.com>
2016-08-19 00:22:00 -0700

    add modal and modal overlay

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-//------------------------------------
-//	Document Ready
-//--------------------------------------
+//-----------------------------------------------
+//   DOCUMENT READY
+//-----------------------------------------------
 
 $(document).ready(function() {
-	$('.js-like').on('click', function(event) {
-    	event.preventDefault();
 
-		$(this).text('Liked!')
-		.closest('.news-item')
-		.addClass('is-liked');
-	});
+  //-----------------------------------------------
+  //   Like
+  //-----------------------------------------------
 
-	$('.js-add-link').on('click', function(event) {
-    	event.preventDefault();
+  $('.js-like').on('click', function(event) {
+    event.preventDefault();
 
-    	$('.js-form').toggleClass('is-visible');
-    });
+    $(this).text('Liked!')
+     .closest('.news-item')
+     .addClass('is-liked');
+  });
 
+  //-----------------------------------------------
+  //   Add Link
+  //-----------------------------------------------
+
+  $('.js-add-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-form').toggleClass('is-visible');
+  });
+
+  //-----------------------------------------------
+  //   Modal
+  //-----------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function() {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
 });
